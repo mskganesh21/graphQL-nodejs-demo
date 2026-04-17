d5a76dcac0af38ae02ff7a9e0900a8f1ece3027e
mskganesh21 <mskganesh21@gmail.com>
2026-04-17 15:23:39 +0530

    added type definitions,queries and mutations for the graphQL

---
+++ schemas/typeDefs.js
@@ -0,0 +1,60 @@
+import {gql} from '@apollo/server';
+
+const typeDefs = gql`
+
+#--------------TYPES--------------
+
+  type User {
+    id: ID!
+    email: String!
+    orders(status: String): [Order]
+    orderCount(status: String): Int
+  }
+  
+  type Product {
+    id: ID!
+    name: String!
+    price: Float!
+    category: String!
+  }
+
+  type Order {
+    id: ID!
+    user: User!
+    status: String!
+    items: [OrderItem!]!
+    total: Float
+    orderPlacedOn: String
+  }
+
+  type OrderItem {
+    id: ID!
+    orderId: ID!
+    productId: ID!
+    quantity: Int!
+    }
+
+#------------- INPUT TYPES-------------
+input CreateUserInput {
+email: String!
+}
+
+#---------------QUERIES----------------------
+type Query {
+users: [User!]!
+user(id:ID!): User
+
+products(category: String): [Product!]!
+
+orders(userId: ID,status: String): [Order!]!
+
+order(id: ID!): Order
+}
+
+#---------------MUTATIONS---------------------
+type Mutation {
+createUser(input: CreateUserInput): User!
+}
+  `;
+
+export default typeDefs;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { connectToDatabase } from "./config/dbConnect.js";
+import { ApolloServer } from "@apollo/server";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.listen(3000, async () => {
   await connectToDatabase();
